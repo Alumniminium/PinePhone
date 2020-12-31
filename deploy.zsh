f7ec323088db71edf9b2dfa6eaaaf6c8bb2c6aca
Your Name <you@example.com>
2020-12-31 22:20:33 +0100

    deploy.zsh

--- deploy.zsh
+++ deploy.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 
 cp -rsf $PWD/.config/* "/home/$(whoami)/.config/"
-mkdir "/home/$(whoami)/coding/"
+mkdir "/home/$(whoami)/coding/" > /dev/null
 cp -rsf $PWD/home/* "/home/$(whoami)/coding/"
-mkdir "/home/$(whoami)/.local"
-mkdir "/home/$(whoami)/.local/bin"
+mkdir "/home/$(whoami)/.local" > /dev/null
+mkdir "/home/$(whoami)/.local/bin" > /dev/null
 cp -rsf $PWD/scripts/* "/home/$(whoami)/.local/bin/"
